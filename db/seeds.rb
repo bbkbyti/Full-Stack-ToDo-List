1c806b1f710c1afeb3f9e5a954e911cfe38250fd
Babak Bayati <bbkbyti@Babaks-MBP.lan>
2022-05-27 15:43:08 -0500

    some data seeded for test purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Weekday.destroy_all
+Todo.destroy_all
+
+Todo.create!(title: 'Buy grocery')
+Todo.create!(title: 'Go to school')
+Todo.create!(title: 'Pay rent')
+Todo.create!(title: 'Buy gas')
+Todo.create!(title: 'Study Math for two hours')
+Todo.create!(title: 'Call mom')
+Todo.create!(title: 'Practice guitar for one hour')
+Todo.create!(title: 'Solve algorithms for one hour')
+Todo.create!(title: 'Practice JavaScript for two hours')
+Todo.create!(title: 'Go to soccer game in the afternoon')
+
+puts "#{Todo.count} todos created!"
+
+Weekday.create!(day: 'Sunday')
+Weekday.create!(day: 'Monday')
+Weekday.create!(day: 'Teusday')
+Weekday.create!(day: 'Wednesday')
+Weekday.create!(day: 'Thursday')
+Weekday.create!(day: 'Friday')
+Weekday.create!(day: 'Saturday')
+
+puts "#{Weekday.count} weekdays created!"
